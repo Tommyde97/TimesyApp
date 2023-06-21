a5a4ddd69b72aeb17db66ea381e10a974b70d002
Tommy <tommyde97@hotmail.com>
2023-06-21 14:50:18 -0400

    Getting Started

---
+++ Timesy/Resources/DatabaseManager.swift
@@ -0,0 +1,61 @@
+//
+//  DatabaseManager.swift
+//  Timesy
+//
+//  Created by Tomas D. De Andrade Gomes on 6/21/23.
+//
+
+import Foundation
+import FirebaseDatabase
+
+final class DatabaseManager {
+    
+    static let shared = DatabaseManager()
+    
+    private let database = Database.database().reference()
+    
+}
+
+// MARK: - Account Management
+
+extension DatabaseManager {
+    
+    public func userExists(with email: String,
+                           completion: @escaping((Bool) -> Void)) {
+        
+        
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
+            guard snapshot.value as? String != nil else {
+                completion(false)
+                return
+            }
+            
+            completion(true)
+        })
+        
+    }
+    
+    /// Inserts new user to database
+    public func insertUser(with user: TimesyAppUser) {
+        database.child(user.safeEmail).setValue([
+            "first_name": user.firstName,
+            "last_name": user.lastName
+        ])
+    }
+}
+
+struct TimesyAppUser {
+    let firstName: String
+    let lastName: String
+    let emailAddres: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddres.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
+    //let profilePictureURL: String
+}
