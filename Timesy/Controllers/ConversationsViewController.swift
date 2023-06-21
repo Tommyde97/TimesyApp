08981c8deffa9c73e82c4bfcccf0adde06a91950
Tommy <tommyde97@hotmail.com>
2023-06-21 14:55:45 -0400

    Wrap up getting started

---
+++ Timesy/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ConversationsViewController.swift
+//  Timesy
+//
+//  Created by Tomas D. De Andrade Gomes on 6/21/23.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
